316e4234e0e130daf2da720fe550b62c1c8c1634
tableminpark <tablemin_park@daum.net>
2025-02-17 22:43:28 +0900

    feat: 개인정보처리방침 생성

--- apps/monglife-discovery-app-auth-api/src/main/java/com/monglife/discovery/app/auth/global/config/DataSourceConfig.java
+++ apps/monglife-discovery-app-auth-api/src/main/java/com/monglife/discovery/app/auth/global/config/DataSourceConfig.java
@@ -1,13 +1,17 @@
 package com.monglife.discovery.app.auth.global.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.SpringBeanContainer;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -35,6 +39,9 @@ public class DataSourceConfig {
     @Value("${spring.jpa.properties.hibernate.show_sql}")
     private String showSql;
 
+    @Value("${spring.jpa.properties.hibernate.format_sql}")
+    private String formatSql;
+
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -46,12 +53,15 @@ public class DataSourceConfig {
         return new HibernateJpaVendorAdapter();
     }
 
-    private Properties jpaProperties() {
+    @Bean
+    public Properties jpaProperties(ConfigurableListableBeanFactory beanFactory) {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", dialect);
-        properties.put("hibernate.hbm2ddl.auto", ddlAuto);
-        properties.put("hibernate.show_sql", showSql);
-        properties.put("hibernate.physical_naming_strategy", "com.monglife.discovery.app.auth.global.config.ImprovedNamingStrategy");
+        properties.put(AvailableSettings.DIALECT, dialect);
+        properties.put(AvailableSettings.HBM2DDL_AUTO, ddlAuto);
+        properties.put(AvailableSettings.SHOW_SQL, showSql);
+        properties.put(AvailableSettings.FORMAT_SQL, formatSql);
+        properties.put(AvailableSettings.PHYSICAL_NAMING_STRATEGY, "com.monglife.discovery.app.auth.global.config.ImprovedNamingStrategy");
+        properties.put(AvailableSettings.BEAN_CONTAINER, new SpringBeanContainer(beanFactory));
         return properties;
     }
 
@@ -64,19 +74,19 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManager() {
+    public LocalContainerEntityManagerFactoryBean entityManager(@Qualifier("jpaProperties") Properties jpaProperties) {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
         em.setPackagesToScan("com.monglife.discovery.app.auth.*.entity");
         em.setJpaVendorAdapter(jpaVendorAdapter());
-        em.setJpaProperties(jpaProperties());
+        em.setJpaProperties(jpaProperties);
         return em;
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    public PlatformTransactionManager transactionManager(@Qualifier("entityManager") LocalContainerEntityManagerFactoryBean entityManager) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManager().getObject());
+        transactionManager.setEntityManagerFactory(entityManager.getObject());
         return transactionManager;
     }
 }
